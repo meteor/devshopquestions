6a8ba7b56b05f7e15a4f26945759fe2f07c985b4
Slava Kim <slava@meteor.com>
2013-11-12 18:10:20 -0800

    Pretty Date

--- devshopquestions.js
+++ devshopquestions.js
@@ -30,6 +30,34 @@ if (Meteor.isClient) {
     if (this.questionTextHandle)
       this.questionTextHandle.stop();
   };
+  Template.question.prettyDate = function () {
+    return relativeDate(this.timestamp);
+  };
+
+  var timeDependency = new Deps.Dependency;
+  var relativeDate = function (then) {
+    timeDependency.depend();
+    var now = new Date;
+    var secondsAgo = Math.floor((now.getTime() - then.getTime()) / 1000);
+    var minutesAgo = Math.floor(secondsAgo / 60);
+    var hoursAgo = Math.floor(minutesAgo / 60);
+    var daysAgo = Math.floor(hoursAgo / 24);
+
+    if (minutesAgo < 1)
+      return "just now";
+    else if (hoursAgo < 1) {
+      return minutesAgo + " minute" + (minutesAgo !== 1 ? "s" : "") + " ago";
+    } else if (daysAgo < 1) {
+      return hoursAgo + " hour" + (hoursAgo !== 1 ? "s" : "") + " ago";
+    } else if (daysAgo < 40) {
+      return daysAgo + " day" + (daysAgo !== 1 ? "s" : "") + " ago";
+    } else
+      return "Long long time ago";
+  };
+
+  Meteor.setInterval(function () {
+    timeDependency.changed();
+  }, 1000);
 }
 
 if (Meteor.isServer) {
@@ -58,3 +86,5 @@ if (Meteor.isServer) {
       });
   });
 }
+
+
