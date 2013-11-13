1afefcf17be4fd7f8a7e05ea78cff20aa6c9d5af
Slava Kim <slava@meteor.com>
2013-11-12 17:45:37 -0800

    Adjust

--- devshopquestions.js
+++ devshopquestions.js
@@ -39,6 +39,7 @@ if (Meteor.isServer) {
         text: "lorem posum realtime ipsum minimongo short one",
         location: "front door",
         timestamp: (new Date),
+        flagged: true,
         poster: {
           name: "Realtime monkey",
           image: "http://upload.wikimedia.org/wikipedia/en/f/f9/Monkey-gun.jpg"
@@ -47,6 +48,7 @@ if (Meteor.isServer) {
         text: "Echo Park excepteur Wes Anderson Pinterest, fugiat trust fund Cosby sweater McSweeney's 3 wolf moon XOXO organic bitters Portland Brooklyn. Put a bird on it Tonx cred, cardigan mustache keytar scenester. Irure laborum occupy Etsy craft beer. Swag Marfa distillery, nulla leggings disrupt gastropub small batch Thundercats dolore hoodie Brooklyn. Cosby sweater Shoreditch try-hard irony gluten-free. Church-key hella kogi, Truffaut enim next level chia pork belly photo booth fap Tonx. Blue Bottle mlkshk nesciunt mixtape sriracha typewriter, locavore retro meggings dolor Cosby sweater fanny pack.",
         location: "roof",
         timestamp: new Date(new Date - 1000*10*60),
+        flagged: false,
         poster: {
           name: "Hipster Hacker",
           image: "https://pbs.twimg.com/profile_images/1263403750/riverscuomo-mustache.jpg"
