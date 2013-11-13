5b74096b4a98b369d5112d43f010378552c1e878
Slava Kim <slava@meteor.com>
2013-11-12 17:44:49 -0800

    Revamp

--- devshopquestions.js
+++ devshopquestions.js
@@ -1,52 +1,58 @@
+// {
+//   text: String,
+//   location: String,
+//   timestamp: EJSON.Date,
+//   flagged: Boolean
+//   poster: {
+//     name: String,
+//     image: String (link)
+//   }
+// }
+Questions = new Meteor.Collection("questions");
+
 if (Meteor.isClient) {
-  Session.setDefault("active-section", "questions");
-
-  Template.header.sections = function () {
-    return [{
-      _id: "questions",
-      label: "Questions",
-      icon: "question-sign"
-    }, {
-      _id: "announcements",
-      label: "Announcements",
-      icon: "bullhorn"
-    }, {
-      _id: "guide",
-      label: "Starter's guide",
-      icon: "book"
-    }];
-  };
-
-  Template.header.active = function () {
-    return Session.equals("active-section", this._id) ? "active" : "";
-  };
-
-  Template.header.events({
-    "click .nav a": function (e) {
-      Session.set("active-section", this._id);
-      e.preventDefault();
-    }
-  });
-
   UI.body.active = function () {
     var templateName = Session.get("active-section");
     return Template[templateName];
   };
 
+  Template.questions.questions = function () {
+    return Questions.find();
+  }
   Template.question.rendered = function () {
     var self = this;
-    var handle = Deps.autorun(function () {
-      $(self.find('.question-text')).more({ length: 300 });
+    self.questionTextHandle = Deps.autorun(function () {
+      var text = Questions.findOne(self.data._id).text;
+      $(self.find('.question-text')).text(text).more({ length: 300 });
     });
-
-    Template.question.destroyed = function () {
-      handle.stop();
-    };
+  };
+  Template.question.destroyed = function () {
+    if (this.questionTextHandle)
+      this.questionTextHandle.stop();
   };
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (!Questions.find().count())
+      _.each([{
+        text: "lorem posum realtime ipsum minimongo short one",
+        location: "front door",
+        timestamp: (new Date),
+        poster: {
+          name: "Realtime monkey",
+          image: "http://upload.wikimedia.org/wikipedia/en/f/f9/Monkey-gun.jpg"
+        }
+      }, {
+        text: "Echo Park excepteur Wes Anderson Pinterest, fugiat trust fund Cosby sweater McSweeney's 3 wolf moon XOXO organic bitters Portland Brooklyn. Put a bird on it Tonx cred, cardigan mustache keytar scenester. Irure laborum occupy Etsy craft beer. Swag Marfa distillery, nulla leggings disrupt gastropub small batch Thundercats dolore hoodie Brooklyn. Cosby sweater Shoreditch try-hard irony gluten-free. Church-key hella kogi, Truffaut enim next level chia pork belly photo booth fap Tonx. Blue Bottle mlkshk nesciunt mixtape sriracha typewriter, locavore retro meggings dolor Cosby sweater fanny pack.",
+        location: "roof",
+        timestamp: new Date(new Date - 1000*10*60),
+        poster: {
+          name: "Hipster Hacker",
+          image: "https://pbs.twimg.com/profile_images/1263403750/riverscuomo-mustache.jpg"
+        }
+      }], function (x) {
+        Questions.insert(x);
+      });
   });
 }
