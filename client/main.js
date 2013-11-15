46efa83b0268e5c2d27fa420ccdfb47b52c3161d
Slava Kim <slava@meteor.com>
2013-11-14 20:55:09 -0800

    Basic form with validation
    no actions yet

--- client/main.js
+++ client/main.js
@@ -6,3 +6,25 @@ Template.answeredQuesions.questions = function () {
   return Questions.find({ answered: true });
 };
 
+Template.form.events({
+  'click button': function (e, templ) {
+    var name = $(templ.find("#name")).val();
+    var location = $(templ.find("#location")).val();
+    var question = $(templ.find("#text")).val();
+
+    if (!name || !location || !question)
+      return true;
+
+    $(templ.find("#name")).val("");
+    $(templ.find("#location")).val("");
+    $(templ.find("#text")).val("");
+
+    e.preventDefault();
+    return false;
+  }
+});
+
+Meteor.startup(function () {
+  Session.setDefault("form-shown", false);
+});
+
